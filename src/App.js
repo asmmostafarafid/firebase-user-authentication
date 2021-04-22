9de0abc321aa77bc42f17fb9b5bc55c7c06c1b0d
rafid365.info@gmail.com <rafid365.info@gmail.com>
2021-04-22 16:16:40 +0600

    First firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photoURL: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then(res => {
+        const { displayName, photoURL, email } = res.User;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser);
+        console.log(displayName, photoURL, email);
+
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then(rse => {
+        const signOutUser = {
+          isSignedIn: false,
+          name: '',
+          photo: '',
+          email: ''
+        }
+        setUser(signOutUser);
+        console.log(rse);
+      })
+      .catch(err => {
+
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> :
+          <button onClick={handleSignIn}>Sign In </button>}
+      {
+        user.isSignedIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt=""></img>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBcfsiqY_f9Iq8K9DLDLtON_mWqFIErufM",
+    authDomain: "ema-jhon-simple-55e2a.firebaseapp.com",
+    projectId: "ema-jhon-simple-55e2a",
+    storageBucket: "ema-jhon-simple-55e2a.appspot.com",
+    messagingSenderId: "553831408827",
+    appId: "1:553831408827:web:e0664fb89ee8a9a0c2b59e",
+    measurementId: "G-PDC3RTGLT5"
+  };
+
+  export default firebaseConfig;
